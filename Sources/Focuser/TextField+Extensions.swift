1be648b0d5e2e198c7d8a998109b031e57ffff95
Augustinas Malinauskas <augustinasmal@gmail.com>
2021-09-01 17:19:11 +0100

    toview

--- Sources/Focuser/TextField+Extensions.swift
+++ Sources/Focuser/TextField+Extensions.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-public extension TextField {
+public extension View {
     func focusedLegacy<T: FocusStateCompliant>(_ focusedField: Binding<T>, equals: T) -> some View {
         modifier(FocusModifier(focusedField: focusedField, equals: equals))
     }
